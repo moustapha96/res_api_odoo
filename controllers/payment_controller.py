6bfc604fa69adda756dede09bf5cf4112986a861
Al hussein Khouma/FullStack Developper <alhussein.khouma@univ-thies.sn>
2024-08-20 16:02:58 +0000

    Update payment_controller.py

--- controllers/payment_controller.py
+++ controllers/payment_controller.py
@@ -117,134 +117,6 @@ class PaymentREST(http.Controller):
             )
 
 
-    @http.route('/api/precommande/<id>/payment/<rang>/<amount>', methods=['GET'], type='http', cors="*", auth='none', csrf=False)
-    def api_create_payment_rang_preorder(self, id , rang, amount):
-        try:
-            order = request.env['sale.order'].sudo().search([ ('id', '=', id) ], limit=1)
-            partner = request.env['res.partner'].sudo().search([('id', '=', order.partner_id.id)], limit=1)
-            company = request.env['res.company'].sudo().search([('id', '=', partner.company_id.id)], limit=1)
-            payment_method = request.env['account.payment.method'].sudo().search([ ( 'payment_type', '=',  'inbound' ) ], limit=1)
-            journal = request.env['account.journal'].sudo().search([('company_id', '=', company.id),  ('type', '=', 'sale') ])
-            payment_method_line_vr = request.env['account.payment.method.line'].sudo().search([
-                    ('payment_method_id', '=', payment_method.id),( 'journal_id', '=', journal.id )], limit=1)
-            user = request.env['res.users'].sudo().search([('id', '=', request.env.uid)], limit=1)
-            if not user or user._is_public():
-                admin_user = request.env.ref('base.user_admin')
-                request.env = request.env(user=admin_user.id)
-
-
-            # enregistrement payment
-            if order :
-
-                account_payment = request.env['account.payment'].sudo().create({
-                    'payment_type': 'inbound',
-                    'partner_type': 'customer',
-                    'partner_id': partner.id,
-                    'amount': amount,
-                    'journal_id': journal.id,
-                    'currency_id': partner.currency_id.id,
-                    'payment_method_line_id': 1,
-                    'payment_method_id': payment_method.id, # inbound
-                    'sale_id': order.id,
-                    'is_reconciled': True,
-                    # 'move_id': new_invoice.id
-                })
-                if account_payment:
-                    account_payment.action_post()
-                    if rang == 1:
-                        if order.first_payment_amount == amount:
-                            order.write({
-                                'first_payment_state': True
-                            })
-                        else:
-                            # Montant versé incorrect
-                            return request.make_response(
-                                json.dumps({'erreur': 'Montant versé incorrect pour le premier paiement'}),
-                                headers={'Content-Type': 'application/json'}
-                            )
-                    elif rang == 2:
-                        if order.second_payment_amount == amount:
-                            order.write({
-                                'second_payment_state': True
-                            })
-                        else:
-                            # Montant versé incorrect
-                            return request.make_response(
-                                json.dumps({'erreur': 'Montant versé incorrect pour le deuxième paiement'}),
-                                headers={'Content-Type': 'application/json'}
-                            )
-                    elif rang == 3:
-                        if order.third_payment_amount == amount:
-                            order.write({
-                                'third_payment_state': True
-                            })
-                        else:
-                            # Montant versé incorrect
-                            return request.make_response(
-                                json.dumps({'erreur': 'Montant versé incorrect pour le troisième paiement'}),
-                                headers={'Content-Type': 'application/json'}
-                            )
-
-
-                    return request.make_response(
-                            json.dumps({
-                                'id': order.id,
-                                'name': order.name,
-                                'partner_id': order.partner_id.id,
-                                'type_sale': order.type_sale,
-                                'currency_id': order.currency_id.id,
-                                'company_id': order.company_id.id,
-                                'commitment_date': order.commitment_date.isoformat(),
-                                'state': order.state,
-                                'invoice_status': order.invoice_status,
-                                'first_payment_date': order.first_payment_date.isoformat() if order.first_payment_date else None,
-                                'second_payment_date': order.second_payment_date.isoformat() if order.second_payment_date else None,
-                                'third_payment_date': order.third_payment_date.isoformat() if order.third_payment_date else None,
-
-                                'first_payment_amount': order.first_payment_amount,
-                                'second_payment_amount': order.second_payment_amount,
-                                'third_payment_amount': order.third_payment_amount,
-
-                                'first_payment_state': order.first_payment_state,
-                                'second_payment_state': order.second_payment_state,
-                                'third_payment_state': order.third_payment_state,
-
-                                'invoice_id': account_payment.move_id.id or None ,
-                                'is_reconciled': account_payment.is_reconciled,
-                                'payment_id': account_payment.id,
-                                'payment_name': account_payment.name,
-                                'sale_order': account_payment.sale_id.id,
-                                'move_id': account_payment.move_id.id,
-                                'move_name': account_payment.move_id.name,
-                                # 'payment_state': new_invoice.payment_state,
-                                # 'invoice_lines': [
-                                #     {
-                                #         'id': invoice_line.id,
-                                #         'product_id': invoice_line.product_id.id,
-                                #         'quantity': invoice_line.quantity,
-                                #         'price_unit': invoice_line.price_unit,
-                                #         'company_id': invoice_line.company_id.id,
-                                #         'currency_id': invoice_line.currency_id.id,
-                                #         'partner_id': invoice_line.partner_id.id,
-                                #         'ref': invoice_line.ref
-                                #     } for invoice_line in new_invoice.invoice_line_ids
-                                # ],
-                            }),
-                            headers={'Content-Type': 'application/json'}
-                        )
-            else:
-                return request.make_response(
-                            json.dumps({'erreur': 'precommande non trouvé' }),
-                            headers={'Content-Type': 'application/json'}
-                        )
-
-        except ValueError as e:
-            return request.make_response(
-                json.dumps({'status': 'error', 'message': str(e)}),
-                headers={'Content-Type': 'application/json'}
-            )
-
-
     @http.route('/api/commande/<id>/payment', methods=['GET'], type='http', cors="*", auth='none', csrf=False)
     # @check_permissions
     def api_create_payment_order(self, id ):
@@ -256,14 +128,15 @@ class PaymentREST(http.Controller):
             _logger.info(f'partner {partner.email} ')
             _logger.info(f'company {company.name} ')
 
-            # journal_facture = request.env['account.journal'].sudo().search([('id', '=', 1) ], limit=1)
-            journal = request.env['account.journal'].sudo().search([('id', '=', 7) ], limit=1)  # type = sale id= 1 & company_id = 1  ==> journal id = 1 / si journal id = 7 : CASH
+            # journal = request.env['account.journal'].sudo().search([('id', '=', 7) ], limit=1)  # type = sale id= 1 & company_id = 1  ==> journal id = 1 / si journal id = 7 : CASH (en local )
+            journal = request.env['account.journal'].sudo().search([('code', '=', 'CSH1'),( 'company_id', '=', company.id ) ], limit=1)  # type = sale id= 1 & company_id = 1  ==> journal id = 1 / si journal id = 7 : CASH
             # sur le vps : le journal pour CASH est 6
             # journal = request.env['account.journal'].sudo().search([('company_id', '=', company.id),  ('type', '=', 'sale') ], limit=1)  # type = sale id= 1 & company_id = 1  ==> journal id = 1 / si journal id = 7 : CASH
             _logger.info(f'JOURNAL {journal.id} ')
             payment_method = request.env['account.payment.method'].sudo().search([ ( 'payment_type', '=',  'inbound' ) ], limit=1) # payement method : TYPE Inbound & id = 1
-            payment_method_line_vr = request.env['account.payment.method.line'].sudo().search([
-                    ('payment_method_id', '=', payment_method.id),( 'journal_id', '=', journal.id )], limit=1) # si journal est cash (id = 7)  et payment method inbound ==> payment method line id  = 1
+            # payment_method_line_vr = request.env['account.payment.method.line'].sudo().search([
+            #         ('payment_method_id', '=', payment_method.id),( 'journal_id', '=', journal.id )], limit=1) # si journal est cash (id = 7)  et payment method inbound ==> payment method line id  = 1
+            # on a pas besoin de payment method line 
 
             _logger.info(f'journal {journal} ')
             _logger.info(f'journal {payment_method} ')
@@ -370,6 +243,7 @@ class PaymentREST(http.Controller):
             )
 
 
+    # methode qu'on utilise
     @http.route('/api/precommande/<id>/payment/<amount>', methods=['GET'], type='http', cors="*", auth='none', csrf=False)
     def api_create_payment_rang_preorder(self, id , amount):
         try:
@@ -381,15 +255,16 @@ class PaymentREST(http.Controller):
             # payment_method_line_vr = request.env['account.payment.method.line'].sudo().search([
             #         ('payment_method_id', '=', payment_method.id),( 'journal_id', '=', journal.id )], limit=1)
 
-            journal = request.env['account.journal'].sudo().search([('id', '=', 6 )] , limit=1) # type = sale id= 1 & company_id = 1  ==> journal id = 1 / si journal id = 7 : CASH
+            journal = request.env['account.journal'].sudo().search([('code', '=', 'CSH1'),( 'company_id', '=', company.id ) ], limit=1)  # type = sale id= 1 & company_id = 1  ==> journal id = 1 / si journal id = 7 : CASH
+            
+            # journal = request.env['account.journal'].sudo().search([('id', '=', 6 )] , limit=1) # type = sale id= 1 & company_id = 1  ==> journal id = 1 / si journal id = 7 : CASH
             payment_method = request.env['account.payment.method'].sudo().search([ ( 'payment_type', '=',  'inbound' ) ], limit=1) # payement method : TYPE Inbound & id = 1
-            payment_method_line_vr = request.env['account.payment.method.line'].sudo().search([ ('payment_method_id', '=', payment_method.id), ( 'journal_id', '=', journal.id ) ], limit=1)  # si journal est cash (id = 7)  et payment method inbound ==> payment method line id  = 1
+            # payment_method_line_vr = request.env['account.payment.method.line'].sudo().search([ ('payment_method_id', '=', payment_method.id), ( 'journal_id', '=', journal.id ) ], limit=1)  # si journal est cash (id = 7)  et payment method inbound ==> payment method line id  = 1
 
             user = request.env['res.users'].sudo().search([('id', '=', request.env.uid)], limit=1)
             if not user or user._is_public():
                 admin_user = request.env.ref('base.user_admin')
                 request.env = request.env(user=admin_user.id)
-
 
             # enregistrement payment
             if order :
