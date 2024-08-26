0feddd4945d691260210ce82062c3ce270751c51
moustapha96 <68778997+moustapha96@users.noreply.github.com>
2024-08-26 13:17:05 +0000

    delete facture

--- models/sale_order_account_move.py
+++ models/sale_order_account_move.py
@@ -1,6 +1,6 @@
 from odoo import models, fields, api
 
-class SaleOrder(models.Model):
+class SaleOrderAccountMove(models.Model):
     _inherit = 'sale.order'
 
     def create_invoice(self):
@@ -8,10 +8,14 @@ class SaleOrder(models.Model):
             # Vérifiez si la commande est confirmée
             if order.state == 'sale':
                 # Créez une facture basée sur la commande de vente
+                journal = self.env['account.journal'].sudo().search([('code', '=', 'sale'), ( 'company_id', '=', order.company.id ) ], limit=1)  # type = sale id= 1 & company_id = 1  ==> journal id = 1 / si journal id = 7 : CASH
+
                 invoice = self.env['account.move'].create({
                     'partner_id': order.partner_id.id,
                     'move_type': 'out_invoice',
                     'invoice_origin': order.name,
+                    'journald_id': journal.id,
+                    'currency_id': journal.currency_id.id, 
                     'invoice_line_ids': [(0, 0, {
                         'product_id': line.product_id.id,
                         'quantity': line.product_uom_qty,
@@ -25,6 +29,6 @@ class SaleOrder(models.Model):
 
     @api.model
     def action_confirm(self):
-        res = super(SaleOrder, self).action_confirm()
+        res = super(SaleOrderAccountMove, self).action_confirm()
         self.create_invoice()
         return res
