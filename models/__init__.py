2c565aa6479fcd63e2e94e58c3b1825d945ca383
moustapha96 <68778997+moustapha96@users.noreply.github.com>
2024-08-26 08:11:33 +0000

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -3,7 +3,7 @@
 from . import rest_api_tokens
 from . import ir_model
 # from . import res_users
-from . import sale_order
+# from . import sale_order
 # from . import res_users_verif
 from . import payment_details
 # from . import account_payment
