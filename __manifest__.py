daff7bd62d45be007445f0f33690c16f76d2a634
moustapha96 <68778997+moustapha96@users.noreply.github.com>
2024-10-01 12:09:47 +0000

    add view

--- __manifest__.py
+++ __manifest__.py
@@ -30,6 +30,7 @@
         'security/ir.model.access.csv',
         'views/ir_model_view.xml',
         'views/product_template_view.xml',
+        'views/res_partner_view.xml'
     ],
     'images': [
         'static/description/banner.png',
